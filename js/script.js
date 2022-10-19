a3ea82af4e6df319f9358fa23551c65e62699f2e
LucaBonifazio <luca.bonifazio13@gmail.com>
2022-10-19 16:27:39 +0200

    fix var

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@
 // Infine scrivi sulla pagina nomecognomecolorepreferito22
 
 
-let name = prompt('Come ti chiami?');
-let surname = prompt('Il tuo cognome?');
-let color = prompt('Il tuo colore preferito?');
-let recap = (`${name} ${surname} ${color}`)
+const name = prompt('Come ti chiami?');
+const surname = prompt('Il tuo cognome?');
+const color = prompt('Il tuo colore preferito?');
+const recap = (`${name} ${surname} ${color}`)
 
 document.getElementById('name').innerHTML = name;
 document.getElementById('surname').innerHTML = surname;
