96e1fd99db0be744e1a39323a2c0efae40a65c4d
LucaBonifazio <luca.bonifazio13@gmail.com>
2022-10-19 15:01:51 +0200

    Complete pass generator

--- js/script.js
+++ js/script.js
@@ -2,3 +2,14 @@
 // poi chiedi il suo cognome,
 // poi chiedi il suo colore preferito
 // Infine scrivi sulla pagina nomecognomecolorepreferito22
+
+
+let name = prompt('Come ti chiami?');
+let surname = prompt('Il tuo cognome?');
+let color = prompt('Il tuo colore preferito?');
+let recap = (`${name} ${surname} ${color}`)
+
+document.getElementById('name').innerHTML = name;
+document.getElementById('surname').innerHTML = surname;
+document.getElementById('color').innerHTML = color;
+document.getElementById('recap').innerHTML = recap;
